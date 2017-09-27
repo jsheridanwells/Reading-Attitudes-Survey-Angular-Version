7ca24e5e52a71753ac4e32c80dbe251a7cd541e8
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-27 15:16:44 -0500

    printing added students to page and to array

--- app/controllers/rosterCreateCtrl.js
+++ app/controllers/rosterCreateCtrl.js
@@ -1,4 +1,26 @@
 'use strict';
-app.controller('rosterCreateCtrl', function () {
-	
+app.controller('rosterCreateCtrl', function ($scope, $window) {
+
+	$scope.studentArr = [];
+	$scope.newStudent = {
+		firstName: '',
+		lastName: '',
+		accessCode: ''
+	};
+
+	$scope.addStudent = () => {
+		let student = $scope.newStudent;
+		$scope.studentArr.push(student);
+		resetNewStudent();
+		console.log("$scope.studentArr", $scope.studentArr);
+	};
+
+	const resetNewStudent = () => {
+			$scope.newStudent = {
+			firstName: '',
+			lastName: '',
+			accessCode: ''
+		};
+	};
+
 });
