1cd1a861a0728c035da311d46cc4b8e386a1ac8f
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-30 09:10:39 -0500

    downloads student objects by user id

--- app/controllers/rosterListCtrl.js
+++ app/controllers/rosterListCtrl.js
@@ -1,5 +1,20 @@
 'use strict';
 
-app.controller('rosterListCtrl', function () {
-	
+app.controller('rosterListCtrl', function ($scope, edFactory, userFactory) {
+
+	//gets uid of logged in user
+	let userId = userFactory.getUserId();
+
+	//list all of the students associated with current user
+	const listAllStudents = () => {
+		console.log("userId", userId);
+		edFactory.getAllStudents(userId)
+			.then(students => {
+				console.log("students from listAllStudents", students);
+			})
+			.catch(error => console.log("error from listAllStudents", error));
+	};
+
+	listAllStudents();
+
 });
--- app/factories/edFactory.js
+++ app/factories/edFactory.js
@@ -5,6 +5,17 @@ app.factory('edFactory', function ($http, $q, FBCreds) {
 	//imports firebase url
 	let url = FBCreds.databaseURL;
 
+	const getAllStudents = (userId) => {
+		return $q((resolve, reject) => {
+			$http.get(`${url}/students.json?orderBy="uid"&equalTo="${userId}"`)
+				.then(students => {
+					console.log("students from getAllStudents", students);
+					resolve(students.data);
+				})
+				.catch(error => reject(error));
+		});
+	};
+
 	//posts new roster object to rosters collection
 	const postStudent = (student) => {
 		let newStudent = angular.toJson(student);
@@ -33,6 +44,7 @@ app.factory('edFactory', function ($http, $q, FBCreds) {
 	};
 
 	return {
+		getAllStudents,
 		postStudent,
 		editStudent,
 		deleteStudent
