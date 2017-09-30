f7daff75693c2f97aebddab2050e6db34cbccec3
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-30 09:28:44 -0500

    prints student list to student list view

--- app/controllers/rosterListCtrl.js
+++ app/controllers/rosterListCtrl.js
@@ -5,12 +5,16 @@ app.controller('rosterListCtrl', function ($scope, edFactory, userFactory) {
 	//gets uid of logged in user
 	let userId = userFactory.getUserId();
 
+	//holds students array
+	$scope.students = [];
+
 	//list all of the students associated with current user
 	const listAllStudents = () => {
 		console.log("userId", userId);
 		edFactory.getAllStudents(userId)
 			.then(students => {
-				console.log("students from listAllStudents", students);
+				$scope.students = students;
+				console.log("students from $scope.students", $scope.students);
 			})
 			.catch(error => console.log("error from listAllStudents", error));
 	};
