32ec3f44a736ee0079c9109a49543edef2f4b3a1
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-02 12:10:01 -0500

    outputs image value on click

--- app/controllers/surveyCtrl.js
+++ app/controllers/surveyCtrl.js
@@ -6,11 +6,15 @@ app.controller('surveyCtrl', function ($scope, $location, studentFactory, survey
 	$scope.count = 0;
 	$scope.surveyQuestions = surveyQuestions;
 
+	let imgValue = 0;
+
 	$scope.setSelected = ($event) => {
 		let el = $event.target;
-		console.log("event", el);
 		resetImgBorders();
 		$(el).attr('class', 'border-selected');
+		imgValue = getImgValue($(el).attr('id'));
+		console.log("imgValue", imgValue);
+
 	};
 
 	$scope.getNext = () => {
@@ -27,6 +31,18 @@ app.controller('surveyCtrl', function ($scope, $location, studentFactory, survey
 
 	const resetImgBorders = () => {
 		$('img').attr('class', 'garfield');
+	};
+
+	const getImgValue = (value) => {
+		if (value === 'one') {
+			return 1;
+		} else if (value === 'two') {
+			return 2;
+		} else if (value === 'three') {
+			return 3;
+		} else if (value === 'four') {
+			return 4;
+		}
 	};
 
 	loadTest();
