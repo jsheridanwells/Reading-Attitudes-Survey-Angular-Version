9a3fd37b1ae14b89d5bfadf27cd03b6058d8c035
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-02 11:39:05 -0500

    prints and advances survey questions from questions array

--- app/controllers/surveyCtrl.js
+++ app/controllers/surveyCtrl.js
@@ -1,8 +1,18 @@
 'use strict';
 
-app.controller('surveyCtrl', function ($scope, $rootScope, studentFactory) {
+app.controller('surveyCtrl', function ($scope, $location, studentFactory, surveyQuestions) {
 
 	$scope.student = studentFactory.getCurrentStudent();
+	$scope.count = 0;
+	$scope.surveyQuestions = surveyQuestions;
+
+	$scope.getNext = () => {
+		if ($scope.count < $scope.surveyQuestions.length - 1) {
+			$scope.count++;
+		} else {
+			$location.url('/#!');
+		}
+	};
 
 	const loadTest = () => {
 		console.log("$scope.student", $scope.student);
